5d364001c2c22e4221ad20dc028a4aec26dd51ee
Konstantin Perikov <pericov.ko@gmail.com>
2022-07-17 14:45:27 +0100

    add implementation

--- preload.js
+++ preload.js
@@ -5,41 +5,4 @@ window.addEventListener('DOMContentLoaded', () => {
         const element = document.getElementById(selector)
         if (element) element.innerText = text
     }
-
-    async function processLineByLine() {
-        const fileStream = fs.createReadStream('input.txt');
-
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
-        // Note: we use the crlfDelay option to recognize all instances of CR LF
-        // ('\r\n') in input.txt as a single line break.
-
-        for await (const line of rl) {
-            // Each line in input.txt will be successively available here as `line`.
-            console.log(`Line from file: ${line}`);
-        }
-    }
-
-
-    // read csv with questions and return based on id
-    function getQuestionById(question) {
-        const fs = require('fs');
-        var i = 1;
-        let questions = new Map();
-        fs.readFileSync('questions.csv', 'utf-8').split(/\r?\n/).forEach(function (line) {
-            questions.set(`question + ${i}`, line);
-            // console.log(line);
-            i++;
-        })
-
-        console.log(questions);
-        return questions.get(question);
-    }
-
-
-    for (const question of ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7']) {
-        replaceText(`${question}`, getQuestionById(question))
-    }
 })
---
+++ renderer.js
@@ -0,0 +1,112 @@
+const nextPageBtn = document.getElementById('nextPage');
+const questionText = document.getElementById('questionText');
+const fs = require('fs');
+
+let currentQuestionId = 0;
+let startTime = 0;
+
+const questions = [
+    "Оцените выраженность следующих признаков в отношении к краснооранжевому новому плоду в форме цветка?",
+    "Оцените выраженность следующих признаков в отношении к новому оранжевому круглому плоду",
+    "Оцените выраженность следующих признаков в отношении к новому коричневому каплеобразному плоду",
+    "Оцените выраженность следующих признаков в отношении к новому зеленому овальному плоду",
+    "Оцените выраженность следующих признаков в отношении к новому зеленому круглому плоду",
+    "Оцените выраженность следующих признаков в отношении к новому красно-зеленому стручковому плоду",
+    "Оцените выраженность следующих признаков в отношении к новому фиолетовому продолговатому плоду",
+    "Оцените выраженность следующих признаков в отношении к бурому вытянутому плоду"
+]
+const propertiesByQuestions = {
+    0: ["Толстая кожура", "Гладкий", "Крупные семена", "Склизский", "Имеет шипы", "Полосатый", "Шершавый"],
+    1: ["Колючий", "Желтая мякоть", "Крупные семена", "Гладкий", "Пористый", "Зернистый", "Маленькие семечки"],
+    2: ["Чешуйчатый", "Шершавый", "Тонкая кожура", "Рассыпчатый", "Ворсистый", "Бугристый", "Склизский"],
+    3: ["Белая мякость", "Ребристый", "Бугристый", "Мягкий", "Сморщенный", "Пятнистый", "Мохнатый"],
+    4: ["Темно-коричневая мякоть", "Тонкая кожура", "Гладкий", "Имеет шипы", "Полый/пустой внутри", "В крапинку", "Шершавый"],
+    5: ["Бархатистый", "Скрученный", "Изогнутый/Красная мякоть", "Гладкий", "Полосатый", "Колючий", "Чешуйчатый"],
+    6: ["Бело-голубая мякоть", "Черные косточки", "Тонкая кожура", "Имеет дольки", "Пятнистый", "Толстая кожура", "Бугристый"],
+    7: ["Зернистый/имеет мякоть в виде \"икринок\"", "Тонкая кожура", "Гладкий", "Коричневые семена", "Мохнатый", "Ребристый", "Колючий"],
+}
+
+function getResults() {
+//    get all checked radio buttons and log their ids
+    let fileName = 'results.log';
+    let checked = [];
+    let radios = document.querySelectorAll("input:checked");
+    for (let i = 0; i < radios.length; i++) {
+        checked.push(radios[i].id);
+    }
+    let endTime = Date.now();
+    let time = endTime - startTime;
+    console.log(checked);
+    console.log(time);
+    startTime = Date.now();
+    fs.appendFileSync(fileName, `${checked.toString()},${time.toString()}\n`);
+}
+
+nextPageBtn.onclick = function () {
+    if (nextPageBtn.innerText === "Завершить тест") {
+        // close file
+
+        //    exit electron app
+        window.close();
+
+    }
+    if (currentQuestionId > 7) {
+        let allClicked = true;
+        for (let i = 1; i <= 7; i++) {
+            let nameSel = `input[name=q${i}]:checked`;
+            let size = document.querySelectorAll(nameSel).length
+            console.log(size);
+            if (size !== 1) {
+                allClicked = false;
+            }
+        }
+        if (allClicked) {
+            getResults();
+            console.log("no more questions to ask");
+            document.getElementById("questionnaire").hidden = true;
+            questionText.innerText = "Спасибо за выполнение задания!";
+            nextPageBtn.innerText = "Завершить тест";
+        }
+    } else if (currentQuestionId === 0) {
+        startTime = Date.now();
+        questionText.innerText = questions[currentQuestionId];
+        properties = propertiesByQuestions[currentQuestionId];
+        for (let i = 1; i <= 7; i++) {
+            document.getElementById(`question${i}`).innerText = properties[i - 1];
+        }
+        document.getElementById("questionnaire").hidden = false;
+        currentQuestionId++;
+    } else {
+        //  if not every radio button group is checked - don't allow to execute code below
+        let allClicked = true;
+        for (let i = 1; i <= 7; i++) {
+            let nameSel = `input[name=q${i}]:checked`;
+            let size = document.querySelectorAll(nameSel).length
+            console.log(size);
+            if (size !== 1) {
+                allClicked = false;
+            }
+        }
+        if (allClicked) {
+            getResults();
+
+            questionText.innerText = questions[currentQuestionId];
+            properties = propertiesByQuestions[currentQuestionId];
+            for (let i = 1; i <= 7; i++) {
+                document.getElementById(`question${i}`).innerText = properties[i - 1];
+            }
+            currentQuestionId++;
+            // uncheck all inputs
+            for (let j = 1; j <= 7; j++) {
+                for (let i = 1; i <= 7; i++) {
+                    document.getElementById(`q${j}_${i}`).checked = false;
+                }
+            }
+            console.log("clicked");
+        } else {
+            console.log("not every radio button is clicked")
+        }
+    }
+}
+
+
